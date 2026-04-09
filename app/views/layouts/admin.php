e09d1025e19ffeb63074c7dff43bff61553842e8
Benjamin-Allegre <allegre.benjamin.pro@gmail.com>
2026-04-09 13:13:30 +0200

    menu dashboard admin

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -11,14 +11,35 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['level_access'] != 1) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title>Dashboard Admin</title>
 </head>
 <body>
+    <header>
+        <!-- NAVIGATION -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+                        <li class="nav-item"><a class="nav-link" href="commerciaux.php">Commerciaux</a></li>
+                        <li class="nav-item"><a class="nav-link" href="clients.php">Clients</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h1>Dashboard Admin</h1>
+        
+        <!-- Inclusion du contenu -->
+        <?php require $content; ?>
+    </main>
+    <footer>
 
-    <h1>Dashboard Admin</h1>
-    
-    <!-- Inclusion du contenu -->
-    <?php require $content; ?>
+    </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$motdepasse = "Benzinho0726";
-$hash = password_hash($motdepasse, PASSWORD_DEFAULT);
-
-echo $hash;
-
-?>
